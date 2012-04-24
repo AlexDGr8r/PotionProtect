2e3952e30102afa94b3699dda49724c8c96db4a9
AlexDGr8r <AlexDGr8r@hotmail.com>
2012-04-24 18:30:34 -0500

    Began work on PotionSplash event (again)

--- src/net/meepcraft/alexdgr8r/potionprotect/PotionListener.java
+++ src/net/meepcraft/alexdgr8r/potionprotect/PotionListener.java
@@ -1,14 +1,26 @@
 package net.meepcraft.alexdgr8r.potionprotect;
 
+import java.util.Collection;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PotionSplashEvent;
+import org.bukkit.potion.PotionEffect;
 
 public class PotionListener implements Listener {
 	
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void potionSplash(PotionSplashEvent event) {
+		Collection<PotionEffect> effects = event.getPotion().getEffects();
+		ProtectPotion proPot = null;
+		for (ProtectPotion potion : Main.protectPotions) {
+			if (effects.size() == potion.potionEffects.size() && effects.containsAll(potion.potionEffects)) {
+				proPot = potion;
+				break;
+			}
+		}
+		if (proPot == null) return;
 		
 	}
 
