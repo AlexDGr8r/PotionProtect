169c16ff3658e3775392a06ec55ec48e22422548
AlexDGr8r <AlexDGr8r@hotmail.com>
2012-04-24 15:55:42 -0500

    Setup configuration and modified ProtectPotion

--- src/net/meepcraft/alexdgr8r/potionprotect/Main.java
+++ src/net/meepcraft/alexdgr8r/potionprotect/Main.java
@@ -1,7 +1,10 @@
 package net.meepcraft.alexdgr8r.potionprotect;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.Potion;
@@ -15,7 +18,10 @@ public class Main extends JavaPlugin {
 	
 	public static WorldEditPlugin worldEdit;
 	public static WorldGuardPlugin worldGuard;
-	public static Potion potion = Potion.fromDamage(16430);
+	public static List<ProtectPotion> protectPotions = new ArrayList<ProtectPotion>();
+	public static int maxPlotsPerPlayer;
+	
+	public FileConfiguration config;
 	
 	public void onEnable() {
 		log = this.getLogger();
@@ -38,7 +44,7 @@ public class Main extends JavaPlugin {
 			log_info(worldGuard.getName() + " Successfully hooked!");
 		}
 		
-		
+		loadConfiguration();
 		
 		log_info("Enabled!");
 	}
@@ -65,6 +71,33 @@ public class Main extends JavaPlugin {
 			return null;
 		}
 		return (WorldGuardPlugin) plugin;
+	}
+	
+	public void loadConfiguration() {
+		config = this.getConfig();
+		List<Integer> potionIDs = config.getIntegerList("PotionIDs");
+		maxPlotsPerPlayer = config.getInt("MaxPlotsPerPlayer", 3);
+		// Load potions with defaults
+		for (int i = 0; i < potionIDs.size(); i++) {
+			Potion potion = Potion.fromDamage(potionIDs.get(i));
+			int h = config.getInt("Potions." + potionIDs.get(i) + ".Height", 256);
+			int w = config.getInt("Potions." + potionIDs.get(i) + ".Width", 10);
+			int l = config.getInt("Potions." + potionIDs.get(i) + ".Length", 10);
+			String perm = config.getString("Potions." + potionIDs.get(i) + ".Permission", "potion." + potionIDs.get(i));
+			protectPotions.add(new ProtectPotion(potion.getEffects(), l, w, h, potionIDs.get(i), perm));
+		}
+		saveConfiguration();
+	}
+	
+	public void saveConfiguration() {
+		config.set("MaxPlotsPerPlayer", maxPlotsPerPlayer);
+		for (ProtectPotion potion : protectPotions) {
+			config.set("Potions." + potion.damageID + ".Height", potion.getHeight());
+			config.set("Potions." + potion.damageID + ".Width", potion.width);
+			config.set("Potions." + potion.damageID + ".Length", potion.length);
+			config.set("Potions." + potion.damageID + ".Permission", potion.permission);
+		}
+		this.saveConfig();
 	}
 
 }
--- src/net/meepcraft/alexdgr8r/potionprotect/ProtectPotion.java
+++ src/net/meepcraft/alexdgr8r/potionprotect/ProtectPotion.java
@@ -1,17 +1,21 @@
 package net.meepcraft.alexdgr8r.potionprotect;
 
+import java.util.Collection;
+
+import org.bukkit.potion.PotionEffect;
+
 public class ProtectPotion {
 	
 	public int damageID;
 	public int width;
 	public int length;
 	public String permission;
-	public String name;
+	public Collection<PotionEffect> potionEffects;
 	
 	private int height; //Here so we can determine the height of the plot based on current Y value
 	
-	public ProtectPotion(String Name, int l, int w, int h, int id, String perm) {
-		name = Name;
+	public ProtectPotion(Collection<PotionEffect> pEffects, int l, int w, int h, int id, String perm) {
+		potionEffects = pEffects;
 		length = l;
 		width = w;
 		height = h;
@@ -19,6 +23,7 @@ public class ProtectPotion {
 		permission = perm;
 	}
 	
+	// Gets maximum height of region based off of Y value
 	public int getMaxHeight(int y) {
 		if (height >= 256 || height <= 0) {
 			return 256;
@@ -27,12 +32,18 @@ public class ProtectPotion {
 		return mHeight > 256 ? 256 : mHeight;
 	}
 	
+	// Gets minimum height of region based off of Y value
 	public int getMinHeight(int y) {
 		if (height >= 256 || height <= 0) {
 			return 0;
 		}
 		int mHeight = y - (height / 2);
 		return mHeight < 0 ? 0 : mHeight;
+	}
+	
+	// For configuration purposes ONLY
+	public int getHeight() {
+		return height;
 	}
 
 }
